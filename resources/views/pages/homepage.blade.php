68982cb0ace9d3d2d6e7c3d48c184fcb6625b8f5
henny <hnytyas72@student.uns.ac.id>
2020-01-16 09:40:57 -0800

    first commit

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+
+@section('main')
+    <div id="homepage">
+        <h2>Homepage</h2>
+    </div>
+@stop
+
+@section('footer')
+    <div id="footer">
+        <p>&copy; 2019 laravelapp.dev</p>
+    </div>
